c1e7e82699c8333adc975da221ea6d5022fa8edf
Allan Caetano de Oliveira <allanoliveira@itstransdata.com>
2020-04-17 22:22:47 -0300

    solução desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,66 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
+}
+
+interface CreateTransactionDTO {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+    private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+    constructor() {
+        this.transactions = [];
+    }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+    public all(): Transaction[] {
+        return this.transactions;
+    }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+    public getBalance(): Balance {
+        const balance = this.transactions.reduce(
+            (accumulator: Balance, transaction: Transaction) => {
+                switch (transaction.type) {
+                    case 'income':
+                        accumulator.income += transaction.value;
+                        accumulator.total += transaction.value;
+                        break;
+                    case 'outcome':
+                        accumulator.outcome += transaction.value;
+                        accumulator.total -= transaction.value;
+                        break;
+                    default:
+                        break;
+                }
+                return accumulator;
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0,
+            },
+        );
 
-  public create(): Transaction {
-    // TODO
-  }
+        return balance;
+    }
+
+    public create({ title, value, type }: CreateTransactionDTO): Transaction {
+        const transaction = new Transaction({
+            title,
+            value,
+            type,
+        });
+
+        this.transactions.push(transaction);
+
+        return transaction;
+    }
 }
 
 export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,38 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+    private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+    constructor(transactionsRepository: TransactionsRepository) {
+        this.transactionsRepository = transactionsRepository;
+    }
 
-  public execute(): Transaction {
-    // TODO
-  }
+    public execute({ title, value, type }: Request): Transaction {
+        if (type === 'outcome') {
+            const { total } = this.transactionsRepository.getBalance();
+
+            if (value > total) {
+                throw Error(
+                    'Outcome value cannot be greater than total balance.',
+                );
+            }
+        }
+
+        const transaction = this.transactionsRepository.create({
+            title,
+            value,
+            type,
+        });
+
+        return transaction;
+    }
 }
 
 export default CreateTransactionService;
